1c5f70f82b4fd3b2928584f575d9b1f80a66b4c4
SO YEON LEE <thdus1323@gmail.com>
2024-04-24 21:19:47 +0900

    기ë기기본세팅_야물

---
+++ src/main/java/com/example/fruitmarket/FruitmarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.fruitmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitmarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FruitmarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fruitmarket/FruitmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fruitmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruitmarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
