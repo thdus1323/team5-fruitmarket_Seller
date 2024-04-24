9b1d6ba229fb703aba6e87a9f686ee16a5706d44
SO YEON LEE <thdus1323@gmail.com>
2024-04-24 21:54:30 +0900

    product 테이블 생성

---
+++ src/main/java/com/example/fruitmarket/Product.java
@@ -0,0 +1,31 @@
+package com.example.fruitmarket;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import java.time.LocalDateTime;
+
+
+@NoArgsConstructor
+@Data
+@Table(name = "product_tb")
+@Entity
+public class Product {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Integer id;
+
+    @Column(unique = true, length = 20, nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer price;
+
+    @Column(nullable = false)
+    private Integer qty;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+}
